fe23101afdeb186106ae63696358d850c7eb6b57
Guy Barker (CELA) <guybark@microsoft.com>
2020-08-05 19:42:53 -0700

    Add project files.

---
+++ WPFAccessKey/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WPFAccessKey
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
